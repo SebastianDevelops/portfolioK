3f697b50d2526671ceb5487a68d8fcc2156481e8
Sebastian Van Rooyen <svanrooyen@ulwembubs.com>
2023-11-21 18:05:42 +0200

    fixed projects

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -59,8 +59,8 @@ const Projects = () => {
       {/* PROJECTS */}
       <div className="flex justify-center">
         <motion.div
-          className="sm:grid sm:grid-cols-3"
-          initial="hidden"
+          className="xs:grid xs:grid-cols-3"
+          initial="visible"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
           transition={{ duration: 0.5 }}
